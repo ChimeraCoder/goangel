dbdd1950cd12c42e8e5e589328516133f83f95ff
Aditya Mukerjee <aditya@quotidianventures.com>
2013-01-28 22:13:35 +0000

    Add basic AngelList queries, with channel-implemented throttling

---
+++ angel/angel.go
@@ -0,0 +1,122 @@
+package angel
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+//For now, assume we already have the access token somehow
+
+const API_BASE = "https://api.angel.co/1"
+
+const SECONDS_PER_QUERY = 10 //By default, execute at most one query every ten seconds
+//Set to 0 to turn off throttling
+
+var queryQueue = make(chan QueryChan, 10)
+
+type QueryChan struct {
+	endpoint_path string
+	keyVals       map[string]string
+	response_ch   chan QueryResponse
+}
+
+type QueryResponse struct {
+	result map[string]interface{}
+	err    error
+}
+
+func init() {
+	go throttledQuery(queryQueue)
+}
+
+func query(endpoint_path string, keyVals map[string]string) (map[string]interface{}, error) {
+
+	endpoint_url := API_BASE + endpoint_path
+
+	v := url.Values{}
+
+	for key, val := range keyVals {
+		v.Set(key, val)
+	}
+
+	resp, err := http.Get(endpoint_url + "?" + v.Encode())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	result := make(map[string]interface{})
+	err = json.Unmarshal(body, &result)
+	return result, err
+}
+
+//Execute a query that will automatically be throttled
+func throttledQuery(queryQueue chan QueryChan) {
+	for q := range queryQueue {
+
+		endpoint_path := q.endpoint_path
+		keyVals := q.keyVals
+		response_ch := q.response_ch
+		result, err := query(endpoint_path, keyVals)
+		response_ch <- struct {
+			result map[string]interface{}
+			err    error
+		}{result, err}
+
+		time.Sleep(SECONDS_PER_QUERY)
+	}
+}
+
+//Query /users/search for a user with the specified slug
+//TODO fix this to search for emails as well
+func QueryUsersSearch(slug string) (*AngelUser, error) {
+	resp_ch := make(chan QueryResponse)
+	queryQueue <- QueryChan{"/users/search", map[string]string{"slug": slug}, resp_ch}
+	r := <-resp_ch
+	res := r.result
+	if err := r.err; err != nil {
+		return nil, err
+	}
+
+	var user AngelUser
+	users_bts, err := json.Marshal(res)
+	if err != nil {
+	}
+	if err := json.Unmarshal(users_bts, &user); err != nil {
+		log.Print(string(users_bts))
+		return nil, err
+	}
+	return &user, nil
+}
+
+/*
+//Query /startup_roles for all startup roles associated with the user with the given id
+func queryStartupRolesAngelList(user_id int64) ([]StartupRole, error) {
+	resp_ch := make(chan QueryResponse)
+	queryQueue <- QueryChan{"/startup_roles", map[string]string{"user_id": strconv.FormatInt(user_id, 10)}, resp_ch}
+	r := <-resp_ch
+	res := r.result
+	if err := r.err; err != nil {
+		return nil, err
+	}
+
+	roles_array := res["startup_roles"].([]interface{})
+
+	var roles []StartupRole
+	roles_bts, err := json.Marshal(roles_array)
+	if err != nil {
+		log.Print("Woah, error occured while marshalling")
+		panic(err)
+	}
+	if err := json.Unmarshal(roles_bts, &roles); err != nil {
+		return nil, err
+	}
+	return roles, err
+}*/
---
+++ angel/angeluser.go
@@ -0,0 +1,27 @@
+package angel
+
+type AngelUser struct {
+	Angellist_url *string
+	Id            int64
+	Name          *string
+
+	Roles []struct { //This corresponds to the "roles" field in AngelList, not the StartupRole
+		Angellist_url *string
+		Display_name  *string
+		Id            int
+		Name          *string
+		Tag_type      *string
+	}
+
+	//StartupRoles []StartupRole
+
+	Linkedin_url   string
+	Bio            string
+	Twitter_url    string
+	Follower_count float64
+	Image          string
+	Facebook_url   string
+	Locations      []interface{}
+	Blog_url       string
+	Online_bio_url string
+}
