9d9119374b08660aeac586bb2eb4c6a67387d2ff
Aditya Mukerjee <aditya@quotidianventures.com>
2013-01-30 20:33:19 +0000

    Fix QueryUsersSearch to use updated throttling function

--- angel/users.go
+++ angel/users.go
@@ -2,7 +2,6 @@ package angel
 
 import (
     "fmt"
-    "encoding/json"
 )
 
 
@@ -28,19 +27,8 @@ func QueryUsersStartups(id int64) ([]StartupRole, error){
 
 //Query /users/search for a user with the specified slug
 //TODO fix this to search for emails as well
-func QueryUsersSearch(slug string) (*AngelUser, error) {
-	resp_ch := make(chan QueryResponse)
-	queryQueue <- QueryChan{"/users/search", map[string]string{"slug": slug}, resp_ch}
-	r := <-resp_ch
-	res := r.result
-	if err := r.err; err != nil {
-		return nil, err
-	}
-
-	var user AngelUser
-	if err := json.Unmarshal(res, &user); err != nil {
-		return nil, err
-	}
-	return &user, nil
+func QueryUsersSearch(slug string) (user AngelUser, err error) {
+    err = execQueryThrottled("/users/search", map[string]string{"slug" : slug}, &user)
+    return
 }
 
